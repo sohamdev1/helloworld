c3263863fd932b4529ab059e7898c3bbd1d45527
sohamdev1 <sohamdevs@yahoo.com>
2023-10-17 03:44:22 +0530

    added build

---
+++ electron/main.js
@@ -0,0 +1,55 @@
+import { app, BrowserWindow, ipcMain } from 'electron'
+import path from 'node:path'
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.js
+// │
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+
+let win = null
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      devTools: process.env.NODE_ENV !== 'production',
+    },
+    width: 640 * 2,
+    height: 480 * 2,
+  })
+  // win.maximize()
+  if (process.env.NODE_ENV !== 'production') {
+    win.webContents.openDevTools()
+  }
+
+  // Test active push message to Renderer-process.
+  // win.webContents.on('did-finish-load', () => {
+  //   win?.webContents.send('main-process-message', new Date().toLocaleString())
+  // })
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL)
+  } else {
+    win.loadFile('dist/index.html')
+    // win.loadFile(path.join(process.env.DIST, 'index.html'))
+  }
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+  win = null
+})
+
+app.whenReady().then(createWindow)
